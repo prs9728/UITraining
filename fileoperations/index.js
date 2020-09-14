eb83eae4cdcce6ed694ac853dbddee35fa8ccb19
Palivela, Ravi Sagar <RaviSagar.Palivela@in.pega.com>
2020-09-14 20:29:23 +0530

    upload

--- fileoperations/index.js
+++ fileoperations/index.js
@@ -1,9 +1,9 @@
 //fs stands for filesystem.
 var file = require('fs');
 //if don't have file, it'll create and write content into file.
-file.writeFileSync("./demo.txt","Srilekha");
+file.writeFileSync("./demo.txt","abc");
 
-file.appendFileSync("./demo.txt"," Kadali");
+file.appendFileSync("./demo.txt"," def");
 
 var dataFromFile = file.readFileSync("./demo.txt").toString();
 console.log(dataFromFile);
